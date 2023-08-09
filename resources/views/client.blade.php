4d8b36c25747bc17b3bb92e30e729c094d6dc184
ambition85 <ambition9385@gmail.com>
2023-08-09 23:29:57 +0900

    update client.blade

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>tmdu_perio</title>
+        <title>Tskr</title>
         <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="icon" href="{{ asset('favicon.ico') }}">
